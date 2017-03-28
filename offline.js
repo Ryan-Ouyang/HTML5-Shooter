a5825c263cd6c5a0e31df07872f6e0d31c8f7a6f
Ryan Ouyang <Ryan Ouyang>
2017-03-28 17:24:29 -0400

    test

---
+++ offline.js
@@ -0,0 +1,30 @@
+﻿{
+	"version": 1490735877,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"images/playerprojectile-sheet0.png",
+		"images/explosion-sheet0.png",
+		"images/explosion-sheet1.png",
+		"images/explosion-sheet2.png",
+		"images/ufo-sheet0.png",
+		"images/gun-sheet0.png",
+		"images/enemyprojectile-sheet0.png",
+		"images/html5_logo_-sheet0.png",
+		"images/adobeflashplayer-sheet0.png",
+		"images/shockwave-sheet0.png",
+		"images/tiledbackground3.png",
+		"images/tiledbackground4.png",
+		"images/tiledbackground.png",
+		"images/tiledbackground2.png",
+		"media/one shot laser fx.ogg",
+		"media/explode fx.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
